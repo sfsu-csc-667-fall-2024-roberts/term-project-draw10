4ff2f69aa9484637e3bd1ace76f9575709105950
Inderpaul-Bhander <ibhander@gmail.com>
2024-12-19 14:19:35 -0800

    index update to handle sockets and updated manifest to export sockets

--- team-draw10/src/server/index.ts
+++ team-draw10/src/server/index.ts
@@ -4,45 +4,55 @@ import express from "express";
 import httpErrors from "http-errors";
 import morgan from "morgan";
 import * as path from "path";
-
-import * as configuration from "./config/manifest";
-import * as routes from "./routes/routemanifest";
-import * as middleware from "./middleware/authentication";
-
+import { createServer } from "http";
 
 dotenv.config();
 
+
+import * as configuration from "./config/manifest";
+import * as routes from "./routes/routemanifest";
+import * as middleware from "./middleware";
+
 const app = express();
+const server = createServer(app);
+
 const PORT = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(process.cwd(), "src",
-"public")));
-app.use(cookieParser());
-app.set("views", path.join(process.cwd(), "src", "server",
-"views"));
-app.set("view engine", "ejs");
+
 
 const staticPath = path.join(process.cwd(), "src", "public");
 app.use(express.static(staticPath));
 
 configuration.configureLiveReload(app, staticPath);
-configuration.configureSession(app);
+configuration.configureSocketID(
+  server,
+  app,
+  configuration.configureSession(app),
+);
+
+app.use(cookieParser());
+app.set("views", path.join(process.cwd(), "src", "server",
+"views"));
+app.set("view engine", "ejs");
+
+app.use(middleware.chat);
 
 app.use("/", routes.root);
 app.use("/auth", routes.auth);
 app.use("/gameslist", routes.gameslist);
-app.use("/creategame", routes.creategame);
-app.use("/gamelobby", routes.gamelobby);
-app.use("/game", routes.game);
+app.use("/creategame", middleware.authentication, routes.creategame);
+app.use("/gamelobby", middleware.authentication, routes.gamelobby);
+app.use("/game", middleware.authentication, routes.game);
 app.use("/gameresult", routes.gameresult);
+app.use("/chats", middleware.authentication, routes.chat);
 
 app.use((_request, _response, next) => {
 	next(httpErrors(404));
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
